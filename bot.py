5504d99b4b1cf9670b2c664ddb6b7a7f22f9009a
淺永瞬 <mokoto0315@gmail.com>
2022-05-26 16:20:46 +0800

    update (bot.py)

--- bot.py
+++ bot.py
@@ -10,23 +10,32 @@ with open('setting.json','r',encoding='utf8') as jfile:
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
-    print(">> 機器人上線 <<")
-    print("登錄系統端:") + (bot.user)
+    print("====== 機器人上線 =======\n")
+    print(" 登錄系統端: " + str(bot.user) +"\n")
+    print(" 系統端版本: " + (jdata['verson']) +"\n")
+    print("=========================")
 
+# 加入訊息
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(int(jdata['Welcome']))
     await channel.send(f'{member} joined!') 
 
+#離開訊息
 @bot.event
 async def on_member_remove(member): 
     channel = bot.get_channel(int(jdata['leave']))
     await channel.send(f'{member} leave!')
 
-
+# 觸發(ping)
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} ms')
+
+# 觸發(圖片)
+@bot.command()
+async def pic(ctx):
+    pic = discord.File('')
 
 
 
