c5a59806631ec14825ec3df01026c5cc330c759d
淺永瞬 <mokoto0315@gmail.com>
2022-05-26 15:02:40 +0800

    create (bot.py)

--- bot.py
+++ bot.py
@@ -6,24 +6,26 @@ with open('setting.json','r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
 
-bot = commands.Bot(command_prefix=".")
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
+    print(bot.user)
 
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(int(jdata['Welcome']))
-    await channel.send(f'{member.name} joined!') 
+    await channel.send(f'{member} joined!') 
 
 @bot.event
 async def on_member_remove(member): 
     channel = bot.get_channel(int(jdata['leave']))
-    await channel.send(f'{member.name} leave!')
+    await channel.send(f'{member} leave!')
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} ms')
+
 
 bot.run(jdata['Token']) 
