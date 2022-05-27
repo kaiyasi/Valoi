40ee960097bb65f330874c8d8844b68fbb954366
淺永瞬 <mokoto0315@gmail.com>
2022-05-27 20:45:59 +0800

    update valoi formal

--- cmds/message.py
+++ cmds/message.py
@@ -13,11 +13,29 @@ with open('setting.json','r',encoding='utf8') as jfile:
 class Event(Cog_extension):
         
 
-# 觸發(文句)
+# 觸發(/v)
     @commands.Cog.listener()
     async def on_message(self,msg):
         if msg.content == '/v':
             await msg.channel.send("當前運行版本: ")+(jdata['verison'])
+
+# 觸發(早安)
+    @commands.Cog.listener()
+    async def on_message(self,msg):
+        if msg.content == '早安':
+            await msg.channel.send("早安阿")
+
+# 觸發(午安)
+    @commands.Cog.listener()
+    async def on_message(self,msg):
+        if msg.content == '午安':
+            await msg.channel.send("吃午餐了嗎")
+
+# 觸發(晚安)
+    @commands.Cog.listener()
+    async def on_message(self,msg):
+        if msg.content == '晚安':
+            await msg.channel.send("該睡了喔")
 
 def setup(bot):
     bot.add_cog(Event(bot))
