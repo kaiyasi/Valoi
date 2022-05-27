47e08ebd17b9f47ee9229bc6724b8a7ceda03376
淺永瞬 <mokoto0315@gmail.com>
2022-05-27 19:12:37 +0800

    create B-1.0.2

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import json 
 import random
 import os
+from datetime import datetime as dt
 
 with open('setting.json','r',encoding='utf8') as jfile: 
     jdata = json.load(jfile)
@@ -12,28 +13,20 @@ with open('setting.json','r',encoding='utf8') as jfile:
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
-    state = ("======== 機器人上線 =========\n"
-    " 登錄系統端: " + str(bot.user) +"\n"
-    " 系統端版本: " + (jdata['verson']) +"\n"
-    "==========================")
+    state = ("===============================\n" 
+    "    當前狀態: :green_circle: 運行中..." + "\n"
+    "    登錄系統端: " + str(bot.user) +"\n"
+    "    系統端版本: " + (jdata['verison']) +"\n"
+    "    當前時間:" + dt.now().strftime("%Y/%m/%d %H:%M:%S")+"\n"
+    "===============================")
     print(state)
     channel = bot.get_channel(int(jdata['data']))
     await channel.send(state)  
 
-# 加入訊息
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(int(jdata['Welcome']))
-    await channel.send(f'{member}加入塔爾小鎮') 
-
-#離開訊息
-@bot.event
-async def on_member_remove(member): 
-    channel = bot.get_channel(int(jdata['leave']))
-    await channel.send(f'{member}因為私人原因必須遷出塔爾小鎮')
-
-
-
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+        
 
 
 if __name__ == "__main__":
--- cmds/main.py
+++ cmds/main.py
@@ -2,25 +2,31 @@
 # 系統預設設定
 import discord
 from discord.ext import commands
-from core.classes import Cog_Extension
+from core.classes import Cog_extension
 import random
 import json
 
 with open('setting.json','r',encoding='utf8') as jfile: 
     jdata = json.load(jfile)
 
-class Main(Cog_Extension):
+class Main(Cog_extension):
         
 # 觸發(ping)
     @commands.command()
     async def ping(self,ctx):
-        await ctx.send(f'{round(self.bot.latency*1000)} ms')
+        await ctx.send(f'當前ping值: {round(self.bot.latency*1000)} /毫秒')
 
 # 觸發(網址圖片)
     @commands.command()
     async def pic(self,ctx):
         random_pic = random.choice(jdata['pic-url'])
-        await self.ctx.send(random_pic)
+        await ctx.send(random_pic)
+
+# 觸發(對話 - Version)
+    @commands.command()
+    async def v(self,ctx):
+        await ctx.send("當前運行版本: "+ jdata['verison'])
+    
 
 
 def setup(bot):
