a29cc3a5b01728beab7bdfd063b835d29e82cb81
淺永瞬 <mokoto0315@gmail.com>
2022-05-27 19:11:56 +0800

    update A-1.0.2

---
+++ cmds/event.py
@@ -0,0 +1,34 @@
+# 指令系統 
+# 系統預設設定
+import discord
+from discord.ext import commands
+from core.classes import Cog_extension
+import random
+import json
+import os
+
+with open('setting.json','r',encoding='utf8') as jfile: 
+    jdata = json.load(jfile)
+
+class Event(Cog_extension):
+        
+# 加入訊息
+    @commands.Cog.listener()
+    async def on_member_join(self,member):
+        channel = self.bot.get_channel(int(jdata['Welcome']))
+        await channel.send(f'⇏ {member.mention} 加入塔爾小鎮 \n 歡迎歡迎') 
+
+#離開訊息
+    @commands.Cog.listener()
+    async def on_member_remove(self,member): 
+        channel = self.bot.get_channel(int(jdata['leave']))
+        await channel.send(f'⇍ {member.mention} 遷出塔爾小鎮 \n 希望可以再度遷回')
+
+# 觸發(文句)
+    @commands.Cog.listener()
+    async def on_message(self,msg):
+        if msg.content == '/v':
+            await msg.channel.send("當前運行版本: ")+(jdata['verison'])
+
+def setup(bot):
+    bot.add_cog(Event(bot))
---
+++ cmds/load.py
@@ -0,0 +1,34 @@
+# 指令系統 
+# 系統預設設定
+import discord
+from discord.ext import commands
+from core.classes import Cog_extension
+import random
+import json
+import os
+
+with open('setting.json','r',encoding='utf8') as jfile: 
+    jdata = json.load(jfile)
+
+class Main(Cog_extension):
+    # 載入/重載/卸載
+
+    @commands.command()
+    async def load(self,ctx,extension):
+        self.bot.load_extension(f'cmds.{extension}')
+        await ctx.send("載入完成...")
+
+    @commands.command()
+    async def unload(self,ctx,extension):
+        self.bot.unload_extension(f'cmds.{extension}')
+        await ctx.send("卸載完成...")
+
+    @commands.command()
+    async def reload(self,ctx,extension):
+        self.bot.reload_extension(f'cmds.{extension}')
+        await ctx.send("重載完成...")
+
+        
+
+def setup(bot):
+    bot.add_cog(Main(bot))
