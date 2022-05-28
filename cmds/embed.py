5f8b93fc22cab5e9f16e60d19e04fb831719ce2e
淺永瞬 <mokoto0315@gmail.com>
2022-05-28 22:35:45 +0800

    Update F-1.0.4

--- cmds/embed.py
+++ cmds/embed.py
@@ -22,7 +22,7 @@ class Main(Cog_extension):
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/979994389021618198/980001844644446218/D2FDFA74-B5A2-4C2C-9C3E-204F04F22E55.jpg")
         embed.add_field(name="✚ 新增", value="• 幹話語錄 \n• 日期指令 \n• 傳送自訂訊息 \n• 刪除訊息 \n• 勘入式訊息", inline=True)
         embed.add_field(name="⟳ 修正", value="• 對話系統 ", inline=False)
-        embed.set_footer(text="Varoi F-1.0.4")
+        embed.set_footer(text="Varoi B-1.0.4")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
