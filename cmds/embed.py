33b2b32bbe8b66323d6a84fc65b436a33336fc09
淺永瞬 <mokoto0315@gmail.com>
2022-05-29 20:11:55 +0800

    Update verison

--- cmds/embed.py
+++ cmds/embed.py
@@ -20,11 +20,25 @@ class Main(Cog_extension):
         embed=discord.Embed(title="Valoi Bot ", description="更新日誌", color=0x70cdf5,timestamp=datetime.datetime.utcnow())
         embed.set_author(name="亞瑟王#1273", icon_url="https://i.imgur.com/br7SNUs.jpg")
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/979994389021618198/980001844644446218/D2FDFA74-B5A2-4C2C-9C3E-204F04F22E55.jpg")
-        embed.add_field(name="✚ 新增", value="• 幹話語錄 \n• 日期指令 \n• 傳送自訂訊息 \n• 刪除訊息 \n• 勘入式訊息", inline=True)
+        embed.add_field(name="✚ 新增", value="• 圖片功能 \n•  \n•  \n•  \n• ", inline=True)
         embed.add_field(name="⟳ 修正", value="• 對話系統 ", inline=False)
+        embed.add_field(name="━  移除", value="• .pic 指令 ", inline=False)
         embed.set_footer(text="Varoi B-1.0.4")
         await ctx.send(embed=embed)
         await ctx.message.delete()
+
+    @commands.command()
+    async def bp(self,ctx):
+        embed=discord.Embed(title="Valoi Bot ", description="男版圖片", color=0x70cdf5,timestamp=datetime.datetime.utcnow())
+        embed.set_author(name="亞瑟王#1273", icon_url="https://i.imgur.com/br7SNUs.jpg")
+        random_pic = random.choice(jdata['pic-url'])
+        embed.set_image(url=f'{random_pic}')
+        
+        await ctx.send(embed=embed)
+        await ctx.message.delete()
+
+
+
 
 def setup(bot):
     bot.add_cog(Main(bot))
